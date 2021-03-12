b0154915e770fd0e8ee498cff058518c54f6e5cb
joewrotehaikus <78707288+joewrotehaikus@users.noreply.github.com>
2021-03-12 00:09:39 -0500

    Add files via upload

--- abacus.js
+++ abacus.js
@@ -1,3 +1,5 @@
+const NUM_ROWS = 15;
+
 let thisRowEquals = (tableId, rowIndex, beadIndex) => {
     if (tableId === 'byOne') {
         return 10**rowIndex * beadIndex;
@@ -6,14 +8,17 @@ let thisRowEquals = (tableId, rowIndex, beadIndex) => {
         if (beadIndex === 0) return 10**rowIndex *  5;
         return 0;
     }
+    return 0;
 }
 
 let totalAbacus = (rows) => {
     let rtnNumber = 0;
-    rows.forEach(row => {
+    rows.forEach((row, index) => {
+        let table = row.parentElement.parentElement;
         blank = row.querySelector('.blank');
-        let [tableId, rowIndex, beadIndex] = identifyBlankBead(blank);
-        rtnNumber += thisRowEquals(tableId, rowIndex, beadIndex);
+        let rowIndex = index >= NUM_ROWS ? index - NUM_ROWS : index;
+        beadIndex = [...row.children].indexOf(blank);
+        rtnNumber += thisRowEquals(table.id, rowIndex, beadIndex);
     });
     return rtnNumber;
 }
@@ -29,13 +34,15 @@ let identifyBlankBead = (beardTD) => {
 let allRows = document.querySelectorAll('tr');
 allRows.forEach(row => {
     row.addEventListener('click', e => {
+        let [tableId, rowNumber, beadNumber] = identifyBlankBead(e.target);
         let row = e.target.parentElement;
         let currentBlank = row.querySelector('.blank');
         currentBlank.classList.remove('blank');
         e.target.classList.add('blank');
-        numberHeader.textContent = totalAbacus(allRows);
+        numberHeader.textContent = totalAbacus(allRows)
+            .toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,');
     });
 });
 
 let numberHeader = document.getElementById('number');
-numberHeader.textContent = totalAbacus(allRows);
+numberHeader.textContent = 0;
